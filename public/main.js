b3ea9b81fe4d04f41ae82f4310906e43e02c1519
as-troska <troska@asvg.no>
2022-11-01 21:02:28 +0100

    Seperated js from html-files

---
+++ public/main.js
@@ -0,0 +1,46 @@
+let games = [];
+let gameTable = document.getElementById("gameTable");
+
+fetch("/getGames")
+    .then((res) => res.json())
+    .then((res) => {
+        for (let x of res) {
+            games.push(x)
+            console.log(x)
+            let row = document.createElement("tr")
+            let cellCheck = document.createElement("td")
+            let cellName = document.createElement("td")
+            let cellKey = document.createElement("td")
+            let checkBox = document.createElement("input")
+            
+            checkBox.type = "checkbox";
+            checkBox.name = "x.slug";
+
+            cellCheck.appendChild(checkBox);
+            row.appendChild(cellCheck)
+
+            cellName.innerHTML = x.name;
+            cellName.dataset.slug = x.slug;
+            cellName.addEventListener("click", drawGame)
+            row.appendChild(cellName)
+
+            cellKey.innerHTML = x.keys[0];
+            row.appendChild(cellKey);
+
+            gameTable.appendChild(row)              
+                             
+            
+        }
+    })
+
+
+function drawGame(evt) {
+    slug = evt.target.dataset.slug
+    fetch("/gameRawg?slug=" + slug)
+    .then((res) => res.json())
+    .then((res) => {
+        let element = document.createElement("p");
+        element.innerHTML = res.description;
+        document.body.appendChild(element);
+    })
+}
